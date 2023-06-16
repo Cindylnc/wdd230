9f79b2c0beaed5dd0c6e07f92a2c5a7842ceca18
Cindylnc <naj22003@byui.edu>
2023-06-15 18:51:57 -0600

    done

--- chamber/scripts/localstorage.js
+++ chamber/scripts/localstorage.js
@@ -1,14 +1,38 @@
+// const userVisit  = document.querySelector(".user-visit");
+// let currentTimestamp = Date.now();
+
+// let lastVisitTime = localStorage.getItem("visits-localstorage");
+// if (lastVisitTime !== null) {
+//   const msForDay = 86400000;
+//   const lastVisitInDays = Math.floor((currentTimestamp - lastVisitTime) / msForDay);
+//   userVisit.textContent = `| 🤗 Welcome back! Your last visit was ${lastVisitInDays} days ago |`;
+// }
+// else {
+//   userVisit.textContent = ` |  👋 Welcome to The PG Chamber!  This is your first visit   |`;
+// }
+
+// localStorage.setItem("visits-localstorage", currentTimestamp);
+
+
+
+
 const userVisit  = document.querySelector(".user-visit");
+
+let lastVisitTime = localStorage.getItem("visits-localstorage"); 
 let currentTimestamp = Date.now();
-
-let lastVisitTime = localStorage.getItem("visits-localstorage");
-if (lastVisitTime !== null) {
-  const msForDay = 86400000;
-  const lastVisitInDays = Math.floor((currentTimestamp - lastVisitTime) / msForDay);
-  userVisit.textContent = `| 🤗 Welcome back! Your last visit was ${lastVisitInDays} days ago |`;
-}
-else {
-  userVisit.textContent = ` |  👋 Welcome to The PG Chamber!  This is your first visit   |`;
-}
-
+const msForDay = 86400000;
+const lastVisitInDays = Math.floor((currentTimestamp - lastVisitTime) / msForDay);
 localStorage.setItem("visits-localstorage", currentTimestamp);
+
+ function calculateDays() {
+    lastVisitInDays;
+    const message = `| 🤗 Welcome back! Your last visit was ${lastVisitInDays} days ago |`;
+    return message;
+}
+
+if (lastVisitInDays >= 1) {
+    userVisit.textContent = calculateDays();
+    } else {
+        userVisit.textContent = ` |  👋 Welcome to The PG Chamber!  This is your first visit   |`;
+     }
+

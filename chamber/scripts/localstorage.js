7464b1ef8d48af7c8b52a93403813971d58db7db
Cindylnc <naj22003@byui.edu>
2023-06-30 23:55:13 -0600

    update localst

--- chamber/scripts/localstorage.js
+++ chamber/scripts/localstorage.js
@@ -16,23 +16,48 @@
 
 
 
-const userVisit  = document.querySelector(".user-visit");
+// const userVisit  = document.querySelector(".user-visit");
 
-let lastVisitTime = localStorage.getItem("visits-localstorage"); 
+// let lastVisitTime = localStorage.getItem("visits-localstorage"); 
+// let currentTimestamp = Date.now();
+// const msForDay = 86400000;
+// const lastVisitInDays = Math.floor((currentTimestamp - lastVisitTime) / msForDay);
+// localStorage.setItem("visits-localstorage", currentTimestamp);
+
+//  function calculateDays() {
+//     lastVisitInDays;
+//     const message = `| 🤗 Welcome back! Your last visit was ${lastVisitInDays} days ago |`;
+//     return message;
+// }
+
+// if (lastVisitInDays >= 1) {
+//     userVisit.textContent = calculateDays();
+//     } else {
+//         userVisit.textContent = ` |  👋 Welcome to The PG Chamber!  This is your first visit   |`;
+//      }
+
+
+
+
+
+
+const userVisit = document.querySelector(".user-visit");
+
+let lastVisitTime = localStorage.getItem("visits-localstorage");
 let currentTimestamp = Date.now();
 const msForDay = 86400000;
+
+if (lastVisitTime === null) {
+localStorage.setItem("visits-localstorage", currentTimestamp);
+userVisit.textContent = " | 👋 Welcome to The PG Chamber! This is your first visit |";
+} else {
 const lastVisitInDays = Math.floor((currentTimestamp - lastVisitTime) / msForDay);
 localStorage.setItem("visits-localstorage", currentTimestamp);
 
- function calculateDays() {
-    lastVisitInDays;
-    const message = `| 🤗 Welcome back! Your last visit was ${lastVisitInDays} days ago |`;
-    return message;
+function calculateDays() {
+const message = `| 🤗 Welcome back! Your last visit was ${lastVisitInDays} days ago |`;
+return message;
 }
 
-if (lastVisitInDays >= 1) {
-    userVisit.textContent = calculateDays();
-    } else {
-        userVisit.textContent = ` |  👋 Welcome to The PG Chamber!  This is your first visit   |`;
-     }
-
+userVisit.textContent = calculateDays();
+}

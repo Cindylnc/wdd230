58d8d2a533f40b31ac31f34683f9ee45ee24dabd
Cindylnc <naj22003@byui.edu>
2023-06-30 23:59:23 -0600

    localstorage update

--- chamber/scripts/localstorage.js
+++ chamber/scripts/localstorage.js
@@ -49,13 +49,13 @@ const msForDay = 86400000;
 
 if (lastVisitTime === null) {
 localStorage.setItem("visits-localstorage", currentTimestamp);
-userVisit.textContent = " | 👋 Welcome to The PG Chamber! This is your first visit |";
+userVisit.textContent = " |   👋 Welcome to The PG Chamber! This is your first visit   |";
 } else {
 const lastVisitInDays = Math.floor((currentTimestamp - lastVisitTime) / msForDay);
 localStorage.setItem("visits-localstorage", currentTimestamp);
 
 function calculateDays() {
-const message = `| 🤗 Welcome back! Your last visit was ${lastVisitInDays} days ago |`;
+const message = `| 🤗 Welcome back! Days since your last visit: ${lastVisitInDays} |`;
 return message;
 }
 

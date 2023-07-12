a759f8857d8d343dfc6e8990eb3b0828fe038813
Cindylnc <naj22003@byui.edu>
2023-07-11 23:18:00 -0600

    header menu pending

--- bountiful/scripts/script.js
+++ bountiful/scripts/script.js
@@ -1,3 +1,12 @@
+// Footer date & last update
 let currentYear = new Date().getFullYear();
 document.querySelector("#year").textContent = currentYear;
 document.querySelector("#lastModifiedDate").textContent = document.lastModified;
+
+// Toggle Menu
+function toggleMenu (){
+    document.getElementById("nav-menu").classList.toggle("open");
+    document.getElementById("hamburgerBtn").classList.toggle("open");
+}
+const hamburgerBtn = document.getElementById("hamburgerBtn");
+hamburgerBtn.addEventListener("click", toggleMenu);
